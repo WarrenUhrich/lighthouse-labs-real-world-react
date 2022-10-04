352336efff05938e2da3ad671fcff4d7283fc383
Warren <hello@warren.codes>
2022-10-04 11:53:04 -0600

    Add nested routes (pokemon) example

--- src/components/router-examples/ExampleReactRouter.jsx
+++ src/components/router-examples/ExampleReactRouter.jsx
@@ -8,6 +8,7 @@ import {
 import Home from './Home';
 import About from './About';
 import NotFound from './NotFound';
+import PlantPokemons from './PlantPokemons';
 
 const ExampleReactRouter = () => {
     return (
@@ -18,11 +19,13 @@ const ExampleReactRouter = () => {
                 <ul>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
+                    <li><Link to="/plant-pokemon">View Plant Pokemon</Link></li>
                 </ul>
             </nav>
             <Routes>
                 <Route path="/" exact element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/plant-pokemon/*" element={<PlantPokemons />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </Router>
